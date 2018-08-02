fc6d8b07f73fbf57f467b8902f1efbc42831acf0
david m flynn <flynndave1@msn.com>
2018-08-02 01:21:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,8 @@ function addElementToEndOfArray(array, element) {
  cities =  addElementToEndOfArray(cities, "New Orleans") */
   
 function  destructivelyAddElementToEndOfArray(array, element) {
-  var temp2 = new array();
+  var temp2 = []
+  temp2 = array();
   return temp2.push(element);
   }
  cities =  destructivelyAddElementToEndOfArray(cities, "Dallas")
